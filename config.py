a3e5310243379f3fb311a17d718698c13f839487
e u p h o r i a <crystvlclevr@icloud.com>
2022-05-18 03:54:13 -0400

    second commit

---
+++ config.py
@@ -0,0 +1,3 @@
+token_path = 'token.json'
+api_key = 'HGMZ7O8IKNYUAQXA87NGP7WIJJ2VKRFC@AMER.OAUTHAP'
+redirect_uri = 'https://localhost'
---
+++ trade.py
@@ -0,0 +1,20 @@
+from tda import auth, client
+import json
+import config
+
+try:
+    c = auth.client_from_token_file(config.token_path, config.api_key)
+except FileNotFoundError:
+    from selenium import webdriver
+    with webdriver.Chrome() as driver:
+        c = auth.client_from_login_flow(
+            driver, config.api_key, config.redirect_uri, config.token_path)
+
+# r = c.get_price_history('AAPL',
+#        period_type=client.Client.PriceHistory.PeriodType.YEAR,
+#        period=client.Client.PriceHistory.Period.TWENTY_YEARS,
+#        frequency_type=client.Client.PriceHistory.FrequencyType.DAILY,
+#        frequency=client.Client.PriceHistory.Frequency.DAILY)
+#assert r.status_code == 200, r.raise_for_status()
+#print(json.dumps(r.json(), indent=4))
+
